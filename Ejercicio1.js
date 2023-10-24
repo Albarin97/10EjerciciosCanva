cbcbd698b9ced4c725c22b721b2eb27fe8c009f9
Albarin97 <albar00@hotmail.com>
2023-10-24 13:55:44 -0600

    Ejercicio 2

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,3 +1,4 @@
+//--------------------------------------Ejercicio 1
 const eje1Btn = document.getElementById('eje1-btn');
 const eje1Inp1 = document.getElementById('eje1-1');
 const eje1Inp2 = document.getElementById('eje1-2');
@@ -16,5 +17,22 @@ eje1Btn.addEventListener('click', () => {
     eje1Out.value = `${array.sort((a, b) => a - b)} | ${array.sort((a, b) => b - a)} | ${array.length!=arraySinRepetidos.length ? 'Repetidos' : ''}`;
 });
 
+//--------------------------------------Ejercicio 2
+
+const eje2Btn = document.getElementById('eje2-btn');
+const eje2Inp1 = document.getElementById('eje2');
+const eje2OutF = document.getElementById('eje2-salidaF');
+const eje2OutK = document.getElementById('eje2-salidaK');
 
 
+eje2Btn.addEventListener('click', ()=>{
+    const celcius = parseFloat(eje2Inp1.value);
+    
+    if (isNaN(celcius)) {
+        alert('Ingresa un número válido en el campo de Celsius.');
+        return; // Sale de la función si no es un número válido
+    }
+
+    eje2OutF.value = `${(celcius*(9/5))+32}°F`;
+    eje2OutK.value = `${celcius+273.15}°K`;
+});
